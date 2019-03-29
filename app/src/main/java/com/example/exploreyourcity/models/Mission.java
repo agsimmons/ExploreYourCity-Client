473bd59365f87fd8d87740a98430a339f6b021de
Andrew Simmons <agsimmons0@gmail.com>
2019-03-29 19:28:01 -0400

    Create Category and Mission classes

---
+++ app/src/main/java/com/example/exploreyourcity/models/Mission.java
@@ -0,0 +1,36 @@
+package com.example.exploreyourcity.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Mission {
+    private int id;
+    private Category category;
+
+    public Mission(int id, Category category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public Mission(JSONObject mission) {
+        try {
+            this.id = mission.getInt("id");
+            this.category = new Category(mission.getJSONObject("category"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    @Override
+    public String toString() {
+        return "Mission(" + this.id + "), " + this.category.toString();
+    }
+}
