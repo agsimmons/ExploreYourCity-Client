e740a4f671adb70403486f40f9975ea6204946e8
Andrew Simmons <agsimmons0@gmail.com>
2019-03-30 16:29:33 -0400

    Update current location automatically

--- app/src/main/java/com/example/exploreyourcity/AvailableMissionListActivity.java
+++ app/src/main/java/com/example/exploreyourcity/AvailableMissionListActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.example.exploreyourcity.adapters.MissionAdapter;
 import com.example.exploreyourcity.models.Mission;
 
@@ -37,12 +38,17 @@ public class AvailableMissionListActivity extends AppCompatActivity {
 
     private void getMissionList() {
 
-        JsonArrayRequest missionListRequest = new JsonArrayRequest(Request.Method.GET,
-                "https://exploreyourcity.xyz/api/missions/",
-                null,
-                new Response.Listener<JSONArray>() {
+        StringRequest missionListRequest = new StringRequest(Request.Method.POST,
+                "https://exploreyourcity.xyz/api/missions/available/",
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(String stringResponse) {
+                        JSONArray response = null;
+                        try {
+                            response = new JSONArray(stringResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                         Log.i("RequestResponse", response.toString());
 
                         ArrayList<Mission> missions = new ArrayList<>();
@@ -78,7 +84,25 @@ public class AvailableMissionListActivity extends AppCompatActivity {
                         Utilities.makeToast(getApplicationContext(), "There was an error with your request");
                     }
                 }) {
+            @Override
+            public Map<String, String> getParams() {
+                SharedPreferences sp = getSharedPreferences("EYCPrefs", Context.MODE_PRIVATE);
 
+                Map<String, String>  params = new HashMap<String, String>();
+
+//                params.put("latitude","42.336113");
+//                params.put("longitude", "-71.095423");
+
+                params.put("latitude", "" + sp.getString("CURRENT_LATITUDE", "0.0"));
+                params.put("longitude", "" + sp.getString("CURRENT_LONGITUDE", "0.0"));
+
+                Log.d("CurrentLocation", "(" + sp.getString("CURRENT_LATITUDE", "0.0") + ", " + sp.getString("CURRENT_LONGITUDE", "0.0") + ")");
+
+                return params;
+
+            }
+
+            @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 SharedPreferences sp = getSharedPreferences("EYCPrefs", Context.MODE_PRIVATE);
 
@@ -90,6 +114,60 @@ public class AvailableMissionListActivity extends AppCompatActivity {
             }
 
         };
+
+//        JsonArrayRequest missionListRequest = new JsonArrayRequest(Request.Method.POST,
+//                "https://exploreyourcity.xyz/api/missions/available/",
+//                requestParameters,
+//                new Response.Listener<JSONArray>() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        Log.i("RequestResponse", response.toString());
+//
+//                        ArrayList<Mission> missions = new ArrayList<>();
+//                        for (int i = 0; i < response.length(); i++) {
+//
+//                            JSONObject missionData = null;
+//
+//                            try {
+//                                missionData = (JSONObject) response.get(i);
+//                            } catch (JSONException e) {
+//                                e.printStackTrace();
+//                            }
+//
+//                            Mission mission = new Mission(missionData);
+//                            missions.add(mission);
+//
+//                        }
+//
+//                        Log.i("Deserialization", missions.toString());
+//
+//                        // Add list elements to RecyclerView
+//                        RecyclerView recyclerView = findViewById(R.id.available_mission_list_recycler_view);
+//                        MissionAdapter missionAdapter = new MissionAdapter(getApplicationContext(), missions);
+//                        recyclerView.setAdapter(missionAdapter);
+//                        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e("Request Error", new String(error.networkResponse.data));
+//                        Utilities.makeToast(getApplicationContext(), "There was an error with your request");
+//                    }
+//                }) {
+//
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                SharedPreferences sp = getSharedPreferences("EYCPrefs", Context.MODE_PRIVATE);
+//
+//                HashMap<String, String> params = new HashMap<>();
+//                String creds = String.format("%s:%s", sp.getString("USERNAME", ""), sp.getString("PASSWORD", ""));
+//                String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.NO_WRAP);
+//                params.put("Authorization", auth);
+//                return params;
+//            }
+//
+//        };
 
         RequestQueueSingleton.getInstance(getApplicationContext()).
                 addToRequestQueue(missionListRequest);
--- app/src/main/java/com/example/exploreyourcity/MapActivity.java
+++ app/src/main/java/com/example/exploreyourcity/MapActivity.java
@@ -1,10 +1,19 @@
 package com.example.exploreyourcity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,6 +21,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -23,9 +36,43 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        // Get location listener set up
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                SharedPreferences.Editor sp_editor = getSharedPreferences("EYCPrefs", Context.MODE_PRIVATE).edit();
+                sp_editor.putString("CURRENT_LATITUDE", "" + location.getLatitude());
+                sp_editor.putString("CURRENT_LONGITUDE", "" +  location.getLongitude());
+                sp_editor.apply();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        // Get Google map set up
         mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // Set onClickListenter for Available Missions button
         Button availableMissionListButton = (Button) findViewById(R.id.map_activity_available_mission_list_button);
         availableMissionListButton.setOnClickListener(new View.OnClickListener() {
             @Override
