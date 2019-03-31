54b9d6438d633174de1bdc5cc9f391b76ffa0a3c
Andrew Simmons <agsimmons0@gmail.com>
2019-03-31 14:25:21 -0400

    Remove old code

--- app/src/main/java/com/example/exploreyourcity/adapters/MissionAdapter.java
+++ app/src/main/java/com/example/exploreyourcity/adapters/MissionAdapter.java
@@ -1,20 +1,14 @@
 package com.example.exploreyourcity.adapters;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.example.exploreyourcity.MissionDetailActivity;
 import com.example.exploreyourcity.R;
-import com.example.exploreyourcity.Utilities;
 import com.example.exploreyourcity.models.Mission;
 
 import java.util.ArrayList;
@@ -24,11 +18,9 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.ViewHold
     private static final String TAG = "MissionAdapter";
 
     private ArrayList<Mission> missions;
-//    private final Context context;
     private OnMissionListener somethingOnMissionListener;
 
     public MissionAdapter(ArrayList<Mission> missions, OnMissionListener onMissionListener) {
-//        this.context = context;
         this.missions = missions;
         this.somethingOnMissionListener = onMissionListener;
     }
@@ -48,20 +40,6 @@ public class MissionAdapter extends RecyclerView.Adapter<MissionAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
         viewHolder.mission_list_element_title.setText(this.missions.get(i).getName());
-
-//        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Mission mission = missions.get(i);
-//                Log.d(TAG, "Clicked on mission " + mission);
-//
-//                // TODO: Fix this
-//                Intent missionDetailIntent = new Intent(context, MissionDetailActivity.class);
-//                missionDetailIntent.putExtra("mission_id", mission.getId());
-//                context.startActivity(missionDetailIntent);
-//            }
-//        });
     }
 
     @Override
